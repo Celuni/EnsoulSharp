933cd9daec248defb1e60750cf23d7d1a4055aaf
Florian Marquardt <mangosd@hotmail.de>
2020-10-20 03:05:53 +0200

    added author name

--- mango.SkinChanger/Properties/AssemblyInfo.cs
+++ mango.SkinChanger/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("mango.SkinChanger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mangosd#9001")]
 [assembly: AssemblyProduct("mango.SkinChanger")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
